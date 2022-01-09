6b123d855b82b19fcf5afaad7f165d57b47424a9
dwestland <don@westland.net>
2022-01-08 20:17:13 -0800

    Update GroupOfMarkersMap page

--- src/components/GroupOfMarkersMap.tsx
+++ src/components/GroupOfMarkersMap.tsx
@@ -5,42 +5,34 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const vectors = [
   { name: 'one', lat: 33.9765, lng: -118.4483 },
-  { name: 'two', lat: 33.9766, lng: -118.4484 },
-  { name: 'three', lat: 33.9767, lng: -118.4485 },
-  { name: 'four', lat: 33.9768, lng: -118.4486 },
+  { name: 'two', lat: 33.9865, lng: -118.4583 },
+  { name: 'three', lat: 33.9965, lng: -118.4683 },
+  { name: 'four', lat: 34.0065, lng: -118.4783 },
 ]
 
-// const vectorArray = []
+const GroupOfMarkersMap = () => {
+  const vectorArray: [number, number][] = vectors.map((vector) => [
+    vector.lat,
+    vector.lng,
+  ])
 
-// const vectorArray = vectors.map((vector) => {
-
-const getLatLngBounds = () => [
-  [33.9765, -118.4483],
-  [33.9766, -118.4484],
-  [33.9767, -118.4485],
-  [33.9768, -118.4486],
-]
-
-const GroupOfMarkersMap = () => (
-  <MapContainer
-    bounds={getLatLngBounds()}
-    // center={[33.9765, -118.4483]}
-    // zoom={14}
-  >
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-
-    {vectors.map((vector) => {
-      console.log(vector.name)
-      return (
+  return (
+    <MapContainer
+      bounds={vectorArray}
+      // center={[33.9765, -118.4483]}
+      // zoom={14}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {vectors.map((vector) => (
         <Marker key={vector.name} position={[vector.lat, vector.lng]}>
           <Popup>{vector.name}</Popup>
         </Marker>
-      )
-    })}
-  </MapContainer>
-)
+      ))}
+    </MapContainer>
+  )
+}
 
 export default GroupOfMarkersMap
