1d6dbb43243eaf7137502d8eb69af0d9cee96d22
dwestland <don@westland.net>
2022-10-06 19:12:15 -0700

    Add leaflet-gesture-handling

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MapContainer, Marker, Popup, TileLayer, Tooltip } from 'react-leaflet'
+import { GestureHandling } from 'leaflet-gesture-handling'
+import * as L from 'leaflet'
+import 'leaflet-gesture-handling/dist/leaflet-gesture-handling.css'
 
-const Map = () => (
-  <MapContainer center={[33.9765, -118.4483]} zoom={14}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[33.9765, -118.4483]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-      <Tooltip direction="bottom" offset={[-10, 40]}>
-        Tooltip for Marker
-      </Tooltip>
-    </Marker>
-  </MapContainer>
-)
+const Map = () => {
+  useEffect(() => {
+    L.Map.addInitHook('addHandler', 'gestureHandling', GestureHandling)
+  })
+
+  return (
+    <MapContainer
+      center={[33.9765, -118.4483]}
+      zoom={14}
+      scrollWheelZoom={false}
+      gestureHandling
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[33.9765, -118.4483]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+        <Tooltip direction="bottom" offset={[-10, 40]}>
+          Tooltip for Marker
+        </Tooltip>
+      </Marker>
+    </MapContainer>
+  )
+}
 
 export default Map
